e8d884d5d31c38ec9f09e109fcb813397d8c55e7
Renato <renatomarquesteles@gmail.com>
2022-07-05 13:14:23 -0300

    adds cart items count on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
 import logo from '../../assets/images/logo.svg';
 import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
+import { Product } from '../../types';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart.reduce<Product[]>((uniqueItems, currentItem) => {
+    if (!uniqueItems.find((item) => item.id === currentItem.id)) {
+      return [...uniqueItems, currentItem];
+    }
+    return uniqueItems;
+  }, []).length;
 
   return (
     <Container>
@@ -20,7 +25,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
