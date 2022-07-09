1c64778a0afc18a7fd6dd7ac6501de0d8b519c7f
Renato <renatomarquesteles@gmail.com>
2022-07-09 00:05:15 -0300

    updates cart functions

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -31,14 +31,16 @@ const Cart = (): JSX.Element => {
     }, 0)
   );
 
-  function handleProductIncrement(product: Product) {}
+  function handleProductIncrement(product: Product) {
+    updateProductAmount({ productId: product.id, amount: product.amount + 1 });
+  }
 
   function handleProductDecrement(product: Product) {
-    // TODO
+    updateProductAmount({ productId: product.id, amount: product.amount - 1 });
   }
 
   function handleRemoveProduct(productId: number) {
-    // TODO
+    removeProduct(productId);
   }
 
   return (
@@ -69,7 +71,7 @@ const Cart = (): JSX.Element => {
                     type="button"
                     data-testid="decrement-product"
                     disabled={product.amount <= 1}
-                    // onClick={() => handleProductDecrement()}
+                    onClick={() => handleProductDecrement(product)}
                   >
                     <MdRemoveCircleOutline size={20} />
                   </button>
@@ -82,7 +84,7 @@ const Cart = (): JSX.Element => {
                   <button
                     type="button"
                     data-testid="increment-product"
-                    // onClick={() => handleProductIncrement()}
+                    onClick={() => handleProductIncrement(product)}
                   >
                     <MdAddCircleOutline size={20} />
                   </button>
